b88c3a836d3667cc9eaa557911266d59f667a048
yunhwan <yhyoon1004@naver.com>
2023-09-30 11:58:46 +0900

    이전 코드 삭제

--- src/main/java/article_guide/article_guide_rest/repository/UserRepository.java
+++
@@ -1,8 +0,0 @@
-package article_guide.article_guide_rest.repository;
-
-import article_guide.article_guide_rest.entity.User;
-import org.springframework.data.jpa.repository.JpaRepository;
-
-public interface UserRepository extends JpaRepository<User, Long> {
-    public User findByUserId(String userId);
-}
