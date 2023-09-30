551e865199477ad2ed4c88c954a226cc1bf404b7
yunhwan <yhyoon1004@naver.com>
2023-09-30 12:45:26 +0900

    시큐리티 기본 설정 + 테스트

--- src/main/java/articleguide/restapi/config/SecurityConfig.java
+++ src/main/java/articleguide/restapi/config/SecurityConfig.java
@@ -1,11 +1,38 @@
 package articleguide.restapi.config;
 
+import jakarta.servlet.DispatcherType;
+import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
+import org.springframework.security.web.SecurityFilterChain;
 
 @Configuration
 @EnableWebSecurity
 public class SecurityConfig {
+    @Bean
+    public SecurityFilterChain securityFilterChain(HttpSecurity httpSecurity) throws Exception {
+        //csrf 설정
+        httpSecurity
+                .csrf(config -> config
+                        .disable()
+                );
 
+        //경로 별 권한 설정
+        httpSecurity
+                .authorizeHttpRequests(config -> config
+                        .dispatcherTypeMatchers(DispatcherType.FORWARD).permitAll()
+                        .anyRequest().permitAll()
+                );
+
+        //로그인 설정
+        httpSecurity.formLogin(loginConfig -> loginConfig
+                .defaultSuccessUrl("/",true)
+                .permitAll()
+        );
+
+
+        return httpSecurity.build();
+    }
 
 }
--- src/main/java/articleguide/restapi/controller/ArticleController.java
+++ src/main/java/articleguide/restapi/controller/ArticleController.java
@@ -2,11 +2,15 @@ package articleguide.restapi.controller;
 
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.ResponseBody;
 import org.springframework.web.bind.annotation.RestController;
 
-@RestController("/article/*")
+@RestController()
+@RequestMapping("/article/*")
 public class ArticleController {
     @GetMapping("test")
+    @ResponseBody
     public String testHandle(){
         return "test OK!";
     }
