36782a4e86074dc0ba98d4568f3ccc92b4ff472c
yunhwan <yhyoon1004@naver.com>
2023-12-25 19:55:04 +0900

    cors 설정

--- src/main/java/articleguide/restapi/config/Config.java
+++ src/main/java/articleguide/restapi/config/Config.java
@@ -1,7 +1,13 @@
 package articleguide.restapi.config;
 
 import org.springframework.context.annotation.Configuration;
+import org.springframework.web.servlet.config.annotation.CorsRegistry;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
 
 @Configuration
-public class Config {
+public class Config implements WebMvcConfigurer {
+    @Override
+    public void addCorsMappings(CorsRegistry registry) {
+        registry.addMapping("/**").allowedMethods("GET,POST,PUT,DELETE".split(","));
+    }
 }
